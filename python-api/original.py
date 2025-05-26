70451e772c9f9f38b97e386fba9d2ad38533da20
codecowboydotio <scottvankalken@gmail.com>
2025-05-26 11:48:12 +1000

    updates

--- python-api/original.py
+++ python-api/original.py
@@ -1,19 +1,5 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import json
-import git
-import asyncio
-from typing import Optional
-from contextlib import AsyncExitStack
-
-from mcp import ClientSession, StdioServerParameters
-from mcp.client.stdio import stdio_client
-
-from anthropic import Anthropic
-from dotenv import load_dotenv
-import os
-import sys
-import shutil
 
 # Initialize Flask application
 app = Flask(__name__)
@@ -32,27 +18,16 @@ def foo():
         return build_cors_preflight_response()
     
     # Process the actual request
-### MATCH KubeResource HAS Label WHERE KubeResource.name CONTAINS 'swapi'
     elif request.method == 'POST':
         # For POST requests, return the data that was sent
         received_data = request.get_json(force=True)
-        policy_name = received_data['entities'][0]['policies'][0]['name']
-        deployment_name = received_data['entities'][0]['policyEvents'][0]['eventLabels']['kubernetes.deployment.name'],
-        git.Repo.clone_from("https://github.com/codecowboydotio/swapi-json-server","/tmp/foo") 
         response = jsonify({
-            #"1-test": received_data['entities'][0]['policies'][0]['name'],
-            "image_name": received_data['entities'][0]['policyEvents'][0]['eventLabels']['container.image.repo'],
-            "deployment_name": received_data['entities'][0]['policyEvents'][0]['eventLabels']['kubernetes.deployment.name'],
             "status": "success",
             "message": "Data received successfully",
-            #"received_data": received_data,
+            "received_data": received_data,
             "data_type": str(type(received_data)),
+            #"test": received_data['foo']
         })
-        try:
-          mydir="/tmp/foo"
-          shutil.rmtree(mydir)
-        except OSError as e:
-          print("Error: %s - %s." % (e.filename, e.strerror))
         return response
     
     else:
